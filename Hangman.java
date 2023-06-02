e9f92d1b6ff70e2efaa1f05965fa366d5bfff1d6
Switxs <134651322+Switxs@users.noreply.github.com>
2023-06-01 21:13:02 -0700

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 
-public class Hangman {
-  private static final String[] words = { "apple", "banana", "cherry", "orange", "strawberry" };
-    private static final int EXTRA_LIFE_THRESHOLD = 5;
+public class Main {
+    private static final int INITIAL_LIVES = 3;
+    private static final int EXTRA_LIFE_THRESHOLD = 4;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String word = getRandomWord();
-        int numLives = calculateInitialLives(word);
-
-        char[] guessedLetters = new char[word.length()];
-        int numLettersGuessed = 0;
-        boolean isGameOver = false;
+        String word;
+        boolean isPlayerOne = true;
 
         System.out.println("Welcome to Hangman!");
-        System.out.println("Guess the word:");
+
+        if (isPlayerOne) {
+            System.out.print("Player 1, enter a word: ");
+            word = scanner.nextLine().toLowerCase();
+            isPlayerOne = false;
+        } else {
+            System.out.println("Player 1 has not entered a word yet. Please wait for Player 1.");
+            return;
+        }
+
+        int numLives = INITIAL_LIVES + word.length() / EXTRA_LIFE_THRESHOLD;
+
+        char[] guessedLetters = new char[word.length()];
+        boolean isGameOver = false;
+
+        System.out.println("Player 2, start guessing!");
+        System.out.println("The word has " + word.length() + " letters.");
 
         while (!isGameOver) {
             displayHangman(numLives);
@@ -23,49 +35,73 @@ public class Hangman {
 
             System.out.print("Enter a letter: ");
             char guess = scanner.nextLine().charAt(0);
-
-            if (isLetterGuessed(guess, guessedLetters)) {
-                System.out.println("You already guessed that letter!");
-                continue;
-            }
+            guess = Character.toLowerCase(guess);
 
             boolean isCorrectGuess = updateGuessedLetters(guess, word, guessedLetters);
             if (isCorrectGuess) {
-                numLettersGuessed++;
                 System.out.println("Correct guess!");
+
+                if (isWordGuessed(guessedLetters)) {
+                    isGameOver = true;
+                    System.out.println("Congratulations! You won!");
+                }
             } else {
                 numLives--;
-                System.out.println("Wrong guess!");
+                System.out.println("Wrong guess! Remaining lives: " + numLives);
 
                 if (numLives == 0) {
                     isGameOver = true;
                     System.out.println("Game Over! You ran out of lives.");
+                    System.out.println("The word was: " + word);
                 }
-            }
-
-            if (numLettersGuessed == word.length()) {
-                isGameOver = true;
-                System.out.println("Congratulations! You won!");
             }
         }
 
         scanner.close();
     }
 
-    private static String getRandomWord() {
-        int index = (int) (Math.random() * words.length);
-        return words[index];
-    }
-
-    private static int calculateInitialLives(String word) {
-        int wordLength = word.length();
-        int numLives = wordLength / EXTRA_LIFE_THRESHOLD;
-        return numLives > 0 ? numLives : 1;
-    }
-
     private static void displayHangman(int numLives) {
-        System.out.println("Remaining lives: " + numLives);
-        // Add your Hangman ASCII art here
+        String[] hangman = {
+                "  +---+",
+                "  |   |",
+                "      |",
+                "      |",
+                "      |",
+                "      |",
+                "======="
+        };
+
+        switch (numLives) {
+            case 6:
+                break;
+            case 5:
+                hangman[2] = "  |   O";
+                break;
+            case 4:
+                hangman[3] = "  |  /|\\";
+                break;
+            case 3:
+                hangman[3] = "  |  /|\\";
+                hangman[4] = "  |   |";
+                break;
+            case 2:
+                hangman[3] = "  |  /|\\";
+                hangman[4] = "  |  /";
+                break;
+            case 1:
+                hangman[3] = "  |  /|\\";
+                hangman[4] = "  |  / \\";
+                break;
+            case 0:
+                hangman[2] = "  |   O";
+                hangman[3] = "  |  /|\\";
+                hangman[4] = "  |  / \\";
+                break;
+        }
+
+        for (String line : hangman) {
+            System.out.println(line);
+        }
     }
 
     private static void displayWordProgress(char[] guessedLetters) {
@@ -76,23 +112,23 @@ public class Hangman {
         System.out.println();
     }
 
-    private static boolean isLetterGuessed(char guess, char[] guessedLetters) {
-        for (char letter : guessedLetters) {
-            if (letter == guess) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     private static boolean updateGuessedLetters(char guess, String word, char[] guessedLetters) {
         boolean isCorrectGuess = false;
         for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == guess) {
+            if (Character.toLowerCase(word.charAt(i)) == guess) {
                 guessedLetters[i] = guess;
                 isCorrectGuess = true;
             }
         }
         return isCorrectGuess;
+    }
+
+    private static boolean isWordGuessed(char[] guessedLetters) {
+        for (char letter : guessedLetters) {
+            if (letter == '\u0000') {
+                return false;
+            }
+        }
+        return true;
     }
 }
